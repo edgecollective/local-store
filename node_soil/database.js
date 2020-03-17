2990a9782997c250e0fb8a4bf7a7f19a2038de46
Don Blair <donblair@gmail.com>
2020-03-17 22:27:02 +0000

    added device name and rssi

--- node_soil/database.js
+++ node_soil/database.js
@@ -20,16 +20,19 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 	    permit FLOAT(8,4), 
 	    bulk FLOAT(8,4),
 	    pore FLOAT(8,4),
-            batt FLOAT(8,2)
+            batt FLOAT(8,2),
+	    deviceName TEXT,
+	    devEUI TEXT,
+	    rssi FLOAT(2,2)
             )`,(err) => {
         if (err) {
             // Table already created
             console.log("already created");
         }else{
             // Table just created, creating some rows
-            var insert = 'INSERT INTO user (dateTime,vwc,temp,permit,bulk,pore,batt) VALUES (?,?,?,?,?,?,?)'
+            var insert = 'INSERT INTO user (dateTime,vwc,temp,permit,bulk,pore,batt,deviceName,devEUI,rssi) VALUES (?,?,?,?,?,?,?,?,?,?)'
             var ts = Math.round((new Date()).getTime() / 1000);
-            db.run(insert, [ts,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+            db.run(insert, [ts,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
             //db.run(insert, [12123124,19.2,23.2])
         }
     })  

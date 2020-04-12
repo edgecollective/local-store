93410fca79c53906050dc8ed45a22a002418aca4
Don Blair <donblair@gmail.com>
2020-04-12 10:16:11 +0000

    fixed time spacing; still need to add lines

--- node_lsn50/plotting/get.js
+++ node_lsn50/plotting/get.js
@@ -1,10 +1,24 @@
-
+function timeConverter(UNIX_timestamp){
+  var a = new Date(UNIX_timestamp * 1000);
+//	console.log(a);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  //var month = months[a.getMonth()];
+  var month = a.getMonth();
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+//var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  var time = year + '-' + month + '-' + date + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
 
 // streaming reference
 var interval = setInterval(function() {
 
 //fetch('http://localhost:8000/api/users/')
-fetch('http://64.227.0.108:8200/api/user/latest')
+fetch('http://64.227.0.108:8500/api/user/latest')
   .then((response) => {
     return response.json();
   })
@@ -13,22 +27,43 @@ fetch('http://64.227.0.108:8200/api/user/latest')
     //console.log(myJson);
 
 var data = myJson.data;
+          console.log(data);
+var xvals = [];	  
 var timestamp = [];
-var adc = [];
-var batv = [];
-var tempc = [];
-var xvals = [];
+var deviceName = [];
+var devEUI = [];
+var rssi = [];
+var batV = [];
+var analog = [];
+var temperature = [];
+
 
 // get the data
 for (i in data) {
   //xvals.push(i);
-  xvals.push(data[i].id);
-  timestamp.push(data[i].timestamp);
-  adc.push(data[i].adc);
-  batv.push(data[i].batv);
-  tempc.push(data[i].tempc);
+  //xvals.push(data[i].id);
+//  xvals.push(data[i].id);
+	console.log(timeConverter(data[i].dateTime));
+  xvals.push(timeConverter(data[i].dateTime));
+  timestamp.push(data[i].dateTime);
+  deviceName.push(data[i].deviceName);
+  devEUI.push(data[i].devEUI);
+  rssi.push(data[i].rssi);
+  batV.push(data[i].batV);
+  analog.push(data[i].analog);
+  temperature.push(data[i].temperature);
 }
 
+// flip b/c of way we got the data form sql:
+
+xvals=xvals.reverse();
+timestamp=timestamp.reverse();
+deviceName=deviceName.reverse();
+devEUI=devEUI.reverse();
+rssi=rssi.reverse();
+batV=batV.reverse();
+analog=analog.reverse();
+temperature=temperature.reverse();
 //console.log(xvals);
 
 // reference for plotly graphing: https://plot.ly/javascript/line-and-scatter/
@@ -36,47 +71,28 @@ for (i in data) {
 // reference for styles: https://plot.ly/javascript/line-and-scatter/
 
 var temp_trace = {
+  //x: xvals.reverse(),
+  x: xvals,  
+ // x: timestamp,
+  y: temperature, 
+  //mode: 'markers',
+  mode: 'markers',
+  type: 'scatter',
+  //marker: { size: 6, color: 'red'}
+};
+
+var rssi_trace = {
+  //x: xvals.reverse(),
   x: xvals,
  // x: timestamp,
-  y: tempc,
+  y: rssi,
   //mode: 'markers',
-  mode: 'lines+markers',
+  mode: 'markers',
   type: 'scatter',
   //marker: { size: 6, color: 'red'}
 };
 
-var batt_trace = {
-  x: xvals, 
-  y: batv,
-  //mode: 'markers',
-  mode: 'lines+markers',
-  type: 'scatter',
-  //marker: { size: 6, color: 'red'}
-};
 
-var humidity_trace = {
-  x: xvals,
-  y: adc,
-  //mode: 'markers',
-  mode: 'lines+markers',
-  type: 'scatter',
-  //marker: { size: 6, color: 'red'}
-};
-
-var layout_humidity = {
-	title:'Volumetric Water Content (%)',
-	yaxis:{
-		title: {
-			text: 'VWC (%)',
-		},
-		range:[0,100]
-	},
-	xaxis: {
-		title: {
-			text: 'index',
-		}
-	}
-};
 var layout_temp = {
 /*   xaxis: {
     range: [ 15, 25 ]
@@ -91,53 +107,49 @@ var layout_temp = {
     title: {
       text: 'Temp (C)',
     },
-	  range: [15,32]
+	  //range: [15,32]
   },
   xaxis: {
     title: {
-      text: 'index',
+      text: 'Time',
     }
   }
 };
 
-var layout_voltage = {
+var layout_rssi = {
 /*   xaxis: {
     range: [ 15, 25 ]
   },
-
+  
   yaxis: {
     range: [15, 25]
-  },
+  }, 
 */
-  title:'Battery Voltage',
+  title:'Signal Strength',
   yaxis: {
     title: {
-      text: 'Voltage (V)',
+      text: 'RSSI',
     },
-	  range: [3,5]
+          //range: [15,32]
   },
   xaxis: {
     title: {
-      text: 'index',
+      text: 'Time',
     }
   }
 };
 
+
+
 var temp_traces = [temp_trace];
-
-var voltage_traces = [batt_trace];
-
-var water_traces = [humidity_trace];
+var rssi_traces = [rssi_trace];
 
 Plotly.newPlot('myDiv_a', temp_traces,layout_temp);
-
-Plotly.newPlot('myDiv_b', voltage_traces,layout_voltage);
-
-Plotly.newPlot('myDiv_c', water_traces,layout_humidity);
+Plotly.newPlot('myDiv_b', rssi_traces,layout_rssi);
 
   });
 
   if(++cnt === 100) clearInterval(interval);
-}, 300);
+}, 1000);
 
 

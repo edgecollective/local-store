1f6cb9bac9a4cf775a75723a4c012ee7aaea0b59
Don Blair <donblair@gmail.com>
2020-05-02 11:22:38 +0000

    update

--- node_lsn50/plotting/get.js
+++ node_lsn50/plotting/get.js
@@ -4,7 +4,7 @@ function timeConverter(UNIX_timestamp){
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
   var year = a.getFullYear();
   //var month = months[a.getMonth()];
-  var month = a.getMonth();
+  var month = a.getMonth()+1;
   var date = a.getDate();
   var hour = a.getHours();
   var min = a.getMinutes();
